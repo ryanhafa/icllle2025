ba90051445c128231eca157d16587ba0e285f015
ryanhafa <ryan.halim@teknokrat.ac.id>
2025-07-26 11:07:41 +0700

    last

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        // Add your custom colors here
+        'my-blue': '#1E3A8A',
+        'brand-primary': '#053946',
+      },
+    },
+  },
+}
